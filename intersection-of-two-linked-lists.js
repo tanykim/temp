b2b35375b2475524bc6bffcdf053337501e85366
Tanyoung Kim <tanykim@gmail.com>
2017-05-09 17:19:33 -0700

    Create intersection-of-two-linked-lists.js

---
+++ intersection-of-two-linked-lists.js
@@ -0,0 +1,38 @@
+/*
+Write a program to find the node at which the intersection of two singly linked lists begins.
+
+
+For example, the following two linked lists:
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+begin to intersect at node c1.
+
+
+Notes:
+
+If the two linked lists have no intersection at all, return null.
+The linked lists must retain their original structure after the function returns.
+You may assume there are no cycles anywhere in the entire linked structure.
+Your code should preferably run in O(n) time and use only O(1) memory.
+*/
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+
+var getIntersectionNode = function(headA, headB) {
+    let nodeA = headA;
+    let nodeB = headB;
+    
+    while (nodeA !== nodeB) {
+        nodeA = (nodeA === null) ? headB : nodeA.next;
+        nodeB = (nodeB === null) ? headA : nodeB.next;
+    }
+    
+    return nodeA;
+};
